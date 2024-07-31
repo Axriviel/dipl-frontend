8b3569a432cadbc3fdd20b18bf4b561d21ef9687
Axriviel <filipv01@jcu.cz>
2024-07-31 14:59:21 +0200

    preparation for notifications

---
+++ src/components/Notification.tsx
@@ -0,0 +1,12 @@
+interface Props {
+    message: string,
+}
+
+//used to diplay any notification for user (e.g. succesfully logged in)
+export const Notification = (props: Props) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
