ee76f4e91d70999816c71b7ec80a7deb97f94b7f
Axriviel <filipv01@jcu.cz>
2024-10-01 17:14:26 +0200

    removed test

--- src/layouts/page/components/Navigation.tsx
+++ src/layouts/page/components/Navigation.tsx
@@ -12,7 +12,7 @@ export const Navigation = () => {
                 <li><Link to="/profile"><Button className="nav-button btn-lg mx-1">Profile</Button></Link></li>
                 <li><Link to="/models"><Button className="nav-button btn-lg mx-1">Models</Button></Link></li>
                 <li><Link to="/modelcreator"><Button className="nav-button btn-lg mx-1">Designer</Button></Link></li>
-                <li><Link to="/test"><Button className="nav-button btn-lg mx-1">test</Button></Link></li>
+                {/* <li><Link to="/test"><Button className="nav-button btn-lg mx-1">test</Button></Link></li> */}
                 <li><Link to="/feedback"><Button className="nav-button btn-lg mx-1">Feedback</Button></Link></li>
                 <li><Link to="/listFeedback"><Button className="nav-button btn-lg mx-1">List Feedback</Button></Link></li>
                 {hasNewNotification ?
