91b6ce58d117081a6ee3f9e4c986f2e32f21686b
Axriviel <filipv01@jcu.cz>
2024-10-04 12:51:03 +0200

    hide nav if not logged in

--- src/layouts/page/components/Navigation.tsx
+++ src/layouts/page/components/Navigation.tsx
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom";
 import { useNotification } from "../../../features/Notifications/NotificationsContext";
 import { useState } from "react";
+import { useAuth } from "../../../features/AuthContext/AuthContext";
 
 export const Navigation = () => {
     const { hasNewNotification } = useNotification();
     const [menuOpen, setMenuOpen] = useState(false);
+    const { isAuthenticated } = useAuth();
 
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
@@ -19,11 +21,15 @@ export const Navigation = () => {
             </div>
             <ul className={`${menuOpen ? "show" : ""}`}>
                 <li><NavLink to="/loginPage" className="nav-button">Login</NavLink></li>
-                <li><NavLink to="/profile" className="nav-button">Profile</NavLink></li>
-                <li><NavLink to="/models" className="nav-button">Models</NavLink></li>
-                <li><NavLink to="/modelcreator" className="nav-button">Designer</NavLink></li>
-                {/* <li><NavLink to="/test"> className="nav-button mx-1">test</NavLink></li> */}
-                <li><NavLink to="/feedback" className="nav-button">Feedback</NavLink></li>
+                {isAuthenticated ?
+                    <>
+                        <li><NavLink to="/profile" className="nav-button">Profile</NavLink></li>
+                        <li><NavLink to="/models" className="nav-button">Models</NavLink></li>
+                        <li><NavLink to="/modelcreator" className="nav-button">Designer</NavLink></li>
+                        {/* <li><NavLink to="/test"> className="nav-button mx-1">test</NavLink></li> */}
+                        <li><NavLink to="/feedback" className="nav-button">Feedback</NavLink></li>
+                    </>
+                    : undefined}
                 <li><NavLink to="/listFeedback" className="nav-button">List Feedback</NavLink></li>
                 {hasNewNotification ?
                     <li><NavLink to="/listNotifications" className="nav-button">New Notifications</NavLink></li> :
