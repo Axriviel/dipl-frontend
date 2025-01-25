76de71fd16f09352442b6a76255750679cc876ca
Axriviel <filipv01@jcu.cz>
2025-01-25 16:16:04 +0100

    fixed button centering

--- src/features/UserAuth/Logout.tsx
+++ src/features/UserAuth/Logout.tsx
@@ -10,7 +10,7 @@ const Logout: React.FC = () => {
     };
 
     return (
-        <div>
+        <div className='d-flex justify-content-center'>
             <Button onClick={handleLogout} className='my-2'>Logout</Button>
         </div>
     );
