591d8ae6000298bbacead2b31a4994d5b7b2b2d9
Axriviel <filipv01@jcu.cz>
2025-02-13 18:57:25 +0100

    add model_name support

--- src/pages/AutoDesignPage/AutoDesignPage.tsx
+++ src/pages/AutoDesignPage/AutoDesignPage.tsx
@@ -21,12 +21,18 @@ export const AutoDesignPage = () => {
             opt_algorithm: "random",
             optimizer: 'adam',
             loss: 'binary_crossentropy',
+            model_name: "myModel",
             metrics: ['accuracy'],
             monitor_metric: "val_accuracy",
             epochs: 10,
             batch_size: 32,
             max_models: 5,
             es_threshold: 0.7,
+            NNI: {
+                nni_concurrency: 1,
+                nni_max_trials: 5,
+                nni_tuner: "Evolution"
+              },
             GA: {
                 generations: 5,
                 populationSize: 5,
